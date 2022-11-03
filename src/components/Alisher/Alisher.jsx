cc63370395f459dd06c6650aa885c74476a328b0
Alisher <zumanovaliser814@gmail.com>
2022-11-03 14:26:59 +0600

    finish registration

--- src/components/Alisher/Alisher.jsx
+++ src/components/Alisher/Alisher.jsx
@@ -1,7 +1,11 @@
 import classes from "./Alisher.module.css";
 
 const Alisher = () => {
-  return <div className={classes.Alisher}>Alisher</div>;
+  return (
+    <div className={classes.Alisher}>
+      <p>Alisher</p>
+    </div>
+  );
 };
 
 export default Alisher;
