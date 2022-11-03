eed9bb86c0dc6b684a0720345aaffbf0310ad0f9
Nurtimax <maksatbeknurtilek@gmail.com>
2022-11-02 19:52:41 -0700

    app jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import Alisher from "./components/alisher/Alisher";
+import Asatbek from "./components/asatbek/Asatbek";
+import Dastan from "./components/dastan/Dastan";
+import Kairat from "./components/kairat/Kairat";
+import Kairatalfa from "./components/Kairatalfa";
+import Malik from "./components/malik/Malik";
+import Nurtilek from "./components/nurtilek/Nurtilek";
 
 function App() {
   return (
     <div className="App">
-      
+      <Alisher />
+      <Asatbek />
+      <Dastan />
+      <Kairat />
+      <Malik />
+      <Nurtilek />
+      <Kairatalfa />
     </div>
   );
 }
